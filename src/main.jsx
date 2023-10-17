4226898dba9248020b68f730c69c3c98adfe22c1
zalish-shawon <zalish.shawon@gmail.com>
2023-10-17 23:15:04 +0600

    routing added

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayouts from './components/mainLayouts/MainLayouts.jsx';
+import Home from './components/Home/Home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayouts></MainLayouts>,
+    
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
