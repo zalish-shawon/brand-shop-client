e1adf099f73014bc3aaac9aad00bc31d2768a51d
zalish-shawon <zalish.shawon@gmail.com>
2023-10-18 00:14:25 +0600

    navbar and hero section added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,28 @@
+import { Link, NavLink } from "react-router-dom";
+import Hero from "../Hero/Hero";
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className="w-[95%] mx-auto">
+            <nav>
+                <div className="navbar bg-base-100">
+                    
+                    <div className="flex-1 mr-4">
+                        <img className="w-[100px]" src="/logo.png" alt="" />
+                    </div>
+                    <div className="flex-none">
+                        <ul className="flex gap-5 items-center font-semibold">
+                            <NavLink to={"/"}><a>Home</a></NavLink>
+                            <NavLink to={"/addProduct"}><a>Add product</a></NavLink>
+                            <NavLink to={"/myCart"}><a>My cart</a></NavLink>
+                            <NavLink to={"/login"}> <button className="btn btn-primary text-white">Login</button> </NavLink>
+                            
+                            
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <Hero></Hero>
         </div>
     );
 };
